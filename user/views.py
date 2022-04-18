39d50b62d7e01af121d1d0d3fc6bc6d29429301d
al.abir313@gmail.com <AbirAlFoysal>
2022-04-18 11:55:40 +0600

    perfectly working from kbook2

--- user/views.py
+++ user/views.py
@@ -4,8 +4,9 @@ from django.contrib import messages
 from django.views.generic import DetailView 
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -13,7 +14,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
+            return redirect('login2')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
